f2a3158a3204b7c43b9e1359f700cfa07b817172
joshlo39 <83348928+joshlo39@users.noreply.github.com>
2023-06-16 22:23:12 -0400

    Adding in M2Challenge chatterBook

---
+++ M2-Challenge-Lopes-Joshua/chatterBook/src/main/java/com/company/chatterBook/ChatterBookApplication.java
@@ -0,0 +1,13 @@
+package com.company.chatterBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatterBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatterBookApplication.class, args);
+	}
+
+}
---
+++ M2-Challenge-Lopes-Joshua/chatterBook/src/main/java/com/company/chatterBook/models/ChatterPost.java
@@ -0,0 +1,23 @@
+package com.company.chatterBook.models;
+
+public class ChatterPost {
+    private String text;
+
+    //constructor
+    public ChatterPost(String text){
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+
+
+
+
+}
