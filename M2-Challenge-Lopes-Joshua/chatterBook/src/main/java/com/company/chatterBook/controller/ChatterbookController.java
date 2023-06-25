23c7728df5f24610cdb79b98363b0404e61659d6
joshlo39 <83348928+joshlo39@users.noreply.github.com>
2023-06-25 17:03:23 -0400

    Cleaning up code and checking against rubric

--- M2-Challenge-Lopes-Joshua/chatterBook/src/main/java/com/company/chatterBook/controller/ChatterbookController.java
+++ M2-Challenge-Lopes-Joshua/chatterBook/src/main/java/com/company/chatterBook/controller/ChatterbookController.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 public class ChatterbookController {
     private List<User> userList;
 
+    // Fake Database
     public ChatterbookController() {
         User luis = new User("Luis");
         User sue = new User("Sue");
